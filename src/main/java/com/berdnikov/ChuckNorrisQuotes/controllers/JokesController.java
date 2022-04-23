dcf9421210bc6fb63e648d8ea045bc1cbb35b58c
Aleksandr Berdnikov <runer@hot.ee>
2022-04-23 16:55:53 +0300

    Refactored: - services package

--- src/main/java/com/berdnikov/ChuckNorrisQuotes/controllers/JokesController.java
+++ src/main/java/com/berdnikov/ChuckNorrisQuotes/controllers/JokesController.java
@@ -1,21 +1,21 @@
 package com.berdnikov.ChuckNorrisQuotes.controllers;
 
-import com.berdnikov.ChuckNorrisQuotes.services.JokesService;
+import com.berdnikov.ChuckNorrisQuotes.services.JokeServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
-    JokesService jokesService;
+    JokeServiceImpl jokeService;
 
-    public JokesController(JokesService jokesService) {
-        this.jokesService = jokesService;
+    public JokesController(JokeServiceImpl jokeService) {
+        this.jokeService = jokeService;
     }
 
     @RequestMapping("/")
     public String getJoke(Model model){
-        model.addAttribute("joke", jokesService.quote());
+        model.addAttribute("joke", jokeService.getJoke());
         return "index";
     }
 }
